2aecebfec5578cad77ff3adff7ad9fe57cbf342a
shenchang97 <846305275@qq.com>
2019-08-27 09:54:34 +0800

    增加 环境aware

--- simple-common/src/main/java/com/sc/registry/center/ZooKeeperServiceDiscovery.java
+++ simple-common/src/main/java/com/sc/registry/center/ZooKeeperServiceDiscovery.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
